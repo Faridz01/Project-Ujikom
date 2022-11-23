f8dace70b8c672984a65f49a6a07bdb1de0ac080
Faridz01 <fariz_241@smkassalaambandung.sch.id>
2022-11-23 08:48:16 +0700

    Tamplating

---
+++ resources/views/hallo.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+
+            <div class="card" style="background-color:#FFFF00;">
+                <div class="card-header">{{ __('Hallo') }}</div>
+                <div class="card-body" style="background-color:#00FFFF;">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    <i>Selamat Datang Di Website</i>  <b>kami</b>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
