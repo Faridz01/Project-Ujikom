85e1bb9edba59b4ed88cf1b9b86d21522ef94abb
Faridz01 <fariz_241@smkassalaambandung.sch.id>
2022-11-23 09:55:44 +0700

    Login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,26 @@
-@extends('layouts.app')
-
+@extends('layouts.master')
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
+            <div class="card" style="background-color:#FFFF00;">
+                <div class="card-header">{{ __('Hallo') }}</div>
+                <div class="card-body" style="background-color:#00FFFF;">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <i>Selamat Datang Di Website</i> <b>{{Auth::user()->name}}</b>
+                    <!-- {{ __('You are logged in!') }} -->
+
                 </div>
             </div>
         </div>
     </div>
 </div>
 @endsection
+ <!-- Hai {{Auth::user()->name}} -->
